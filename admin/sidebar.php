02e49dccaec068990f338de2f09f5f4764794a39
aswin-max <aswinrajeevachu003@gmail.com>
2024-01-10 00:50:19 +0530

    as

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -98,6 +98,20 @@ $username=$_SESSION['username'];
                                 </div>
                             </li>
                             <li class="nav-item">
+                                <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>VEHICLE TYPE</a>
+                                <div id="submenu-2" class="collapse submenu" style="">
+                                    <ul class="nav flex-column">
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="type.php">Vehicle Type <span class="badge badge-secondary">New</span></a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="viewoffence.php">Fuel Type</a>
+                                        </li>
+                    
+                                    </ul>
+                                </div>
+                            </li>
+                            <li class="nav-item">
                                 <a class="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i class="fa fa-fw fa-rocket"></i>OFFENCE</a>
                                 <div id="submenu-2" class="collapse submenu" style="">
                                     <ul class="nav flex-column">
--- admin/type.php
+++ admin/type.php
@@ -1,89 +1,64 @@
-<?php 
-
- require('../config/autoload.php'); 
-include("header.php");
-$file=new FileUpload();
-$elements=array(
-        "tname"=>"");
-
-
-$form=new FormAssist($elements,$_POST);
-
-
-
-$dao=new DataAccess();
-
-$labels=array('tname'=>"tname");
-
-$rules=array(
-    "tname"=>array("required"=>true,"alphaonly"=>true)
-
-
-     
-);
-    
-    
-$validator = new FormValidator($rules,$labels);
-
-if(isset($_POST["insert"]))
-{
-
-if($validator->validate($_POST))
-{
-	
-
-
-$data=array(
-
-       
-
-        'tname'=>$_POST['tname'],
-         
-    );
-
-    print_r($data);
-  
-    if($dao->insert($data,"type"))
-    {
-        echo "<script> alert('New record created successfully');</script> ";
-
-    }
-    else
-        {$msg="Registration failed";} ?>
-
-
-
 <?php
-    
+include("sidebar.php");
+$file = new FileUpload();
+$dao = new DataAccess();
+
+$rules = array(
+    "tname" => array("required" => true, "alphaonly" => true)
+);
+
+$validator = new FormValidator($rules);
+$successMessage = ""; // Initialize the variable to hold the success message
+
+if (isset($_POST["insert"])) {
+
+    if ($validator->validate($_POST)) {
+
+        $data = array(
+            'tname' => $_POST['tname'],
+        );
+
+        if ($dao->insert($data, "type")) {
+            $successMessage = 'New record created successfully';
+            $_POST['tname'] = ''; // Clear the text box value in $_POST
+        } else {
+            $msg = "Registration failed";
+        }
+    } else {
+        echo $file->errors();
+    }
 }
-else
-echo $file->errors();
-}                                                                                                                                                                                                                                                                                                                            
 ?>
 <html>
 <head>
+    <script>
+        // JavaScript function to clear the success message
+        function clearSuccessMessage() {
+            document.getElementById('successMessage').style.display = 'none';
+        }
+    </script>
+    <!-- Include any additional head elements such as stylesheets, scripts, etc. -->
 </head>
 <body>
 
- <form action="" method="POST" enctype="multipart/form-data">
+<?php
+// Check if the successMessage variable is set and not empty
+if (!empty($successMessage)) {
+    echo "<div id='successMessage' style='color: green;'>{$successMessage}</div>";
+}
+?>
 
+<form action="" method="POST" enctype="multipart/form-data">
+    <div class="row">
+        <div class="col-md-6">
+            tname:
+            <input type="text" name="tname" class="form-control" value="<?= isset($_POST['tname']) ? htmlspecialchars($_POST['tname']) : ''; ?>" oninput="clearSuccessMessage()">
+            <?= $validator->error('tname'); ?>
+        </div>
+    </div>
 
-
-<div class="row">
-                    <div class="col-md-6">
-tname:
-
-<?= $form->textBox('tname',array('class'=>'form-control')); ?>
-<?= $validator->error('tname'); ?>
-
-</div>
-</div>
-
-
-<button type="submit" name="insert">Submit</button>
+    <button type="submit" name="insert">Submit</button>
 </form>
 
-
 </body>
-
 </html>
